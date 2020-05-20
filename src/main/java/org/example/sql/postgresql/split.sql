96277e674ad05cabba0322c3e4c3276606e2e5e7
xupp <ping2428572>
2020-05-20 11:33:48 +0800

    添加pg 字符串分割的功能脚本

---
+++ src/main/java/org/example/sql/postgresql/split.sql
@@ -0,0 +1,24 @@
+/**
+  分割字符串
+    f_string :源字符串
+    f_delimiter : 分割字符
+  */
+create or replace function split(
+    IN f_string text,
+    IN f_delimiter varchar(10)
+) returns setof text as
+$ytt$
+declare cnt int;
+    declare i int;
+    declare v_result text;
+begin
+    i := 1;
+    cnt := length(f_string) - length(replace(f_string,f_delimiter,''))+1;
+    while i <= cnt
+        loop
+            v_result := split_part(f_string,f_delimiter,i);
+            return next v_result;
+            i := i + 1;
+        end loop;
+end;
+$ytt$ language plpgsql;
